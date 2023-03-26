baeb0c3eb71f48b7f3517ee21e4ad5924529996e
Daksh-raj <medhavisharma300@gmail.com>
2023-03-26 18:35:49 +0530

    added:

--- sahyog/settings.py
+++ sahyog/settings.py
@@ -173,4 +173,4 @@ STATICFILES_DIRS = [
 
 import os
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
