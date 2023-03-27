ac192b0a13c3e02ef20bf4f51ff7a6881fc2e4eb
Daksh Raj <98844816+Daksh-raj@users.noreply.github.com>
2023-03-27 13:18:33 +0530

    Update settings.py

--- sahyog/settings.py
+++ sahyog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-un1^&c9+y&i&r)(h!4yp_b%_01)=uegr#24l#lnjcxe(i4682)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["dakshraj.pythonanywhere.com"]
 
 
 # Application definition
